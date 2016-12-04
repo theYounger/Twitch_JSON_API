97967f90f58bf1a85b54b2ac3467d50ed5bad2d8
kyle uyehara <kuyehara1@gmail.com>
2016-12-03 19:50:50 -1000

    implement functional ajax call to api

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 $(document).ready(function onReady() {
-   function loadResults(data) {
-      console.log(data);
-   }
 
-   $.ajax({
-      type: "GET",
-      url: "https://wind-bow.hyperdev.space/twitch-api",
-      dataType: "json",
-      success: loadResults
-   });
+   function getTwitchInfo() {
+      function loadResults(data) {
+         console.log(data);
+      }
+
+      function errAjax(obj) {
+         console.error(obj.getAllResponseHeaders());
+      }
+
+      var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+      $.ajax({
+         accepts: {
+           v3: "application/vnd.twitchtv.v3+json",
+         },
+         type: "GET",
+         url: "https://wind-bow.hyperdev.space/twitch-api/users/ESL_SC2",
+         dataType: "jsonp",
+         error: errAjax,
+         success: loadResults,
+      });
+   }
+   getTwitchInfo();
 });
